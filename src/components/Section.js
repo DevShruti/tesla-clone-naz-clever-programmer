f2e9d488fe8d68eac476204f8b3fb25882ff11b9
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-18 15:49:07 -0600

    work on sections, home page looking good

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,6 +8,7 @@ function Section() {
                 <h1>Model S</h1>
                 <p>Order Online for Touchless Delivery</p>
             </ItemText>
+            <Buttons>
             <ButtonGroup>
                 <LeftButton>
                    Custom Order
@@ -16,6 +17,8 @@ function Section() {
                    Existing Inventory
                 </RightButton>
             </ButtonGroup>
+            <DownArrow src="./images/down-arrow.svg" />
+            </Buttons>
         </Wrap>
     )
 }
@@ -30,6 +33,10 @@ const Wrap = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-image: url('./images/model-s.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `
 const ItemText = styled.div`
 padding-top: 15vh;
@@ -37,6 +44,8 @@ text-align: center;
 
 `
 const ButtonGroup = styled.div`
+ display: flex;
+ margin-bottom: 30px;
 `
 const LeftButton = styled.div`
  background-color:rgba(23, 26, 32, 0.8);
@@ -50,6 +59,13 @@ const LeftButton = styled.div`
  opacity: 0.85;
  text-transform: uppercase;
  font-size: 12px;
+ cursor: pointer;
 `
 const RightButton = styled(LeftButton)`
+`
+const DownArrow = styled.img`
+  margin-top:20px;
+  height: 40px;
+`
+const Buttons = styled.div `
 `
